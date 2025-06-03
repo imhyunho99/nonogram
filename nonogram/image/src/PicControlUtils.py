d8dfa7b185435a1b7dc400b52c4476df224c6132
nahyunho99 <nahyunho1999@gmail.com>
2025-06-03 16:15:36 +0900

    add requests.txt

--- nonogram/image/src/PicControlUtils.py
+++ nonogram/image/src/PicControlUtils.py
@@ -8,35 +8,35 @@ from io import BytesIO
 class NonogramUtils:
     @staticmethod
     def make_image(size, image: Image.Image) -> Image.Image:
-
+        # 1. 흑백 변환 및 이진화
         image = image.resize((size, size))
         gray = image.convert("L")
         bw = gray.point(lambda x: 1 if x > 128 else 0, '1')  # 흑 0 / 백 1
         array = np.array(bw, dtype=int)
 
-
+        # 2. 힌트 계산
         row_hints = [NonogramUtils._get_hint(row) for row in array]
         col_hints = [NonogramUtils._get_hint(col) for col in array.T]
 
-
+        # 3. 힌트 영역 크기 결정
         max_row_hint = max(len(h) for h in row_hints)
         max_col_hint = max(len(h) for h in col_hints)
 
         total_height = size + max_col_hint
         total_width = size + max_row_hint
 
-
+        # 4. 전체 그리드 그리기
         fig, ax = plt.subplots(figsize=(total_width / 2, total_height / 2))
         ax.set_xlim(0, total_width)
         ax.set_ylim(0, total_height)
         ax.set_xticks([])
         ax.set_yticks([])
-        ax.invert_yaxis()
+        ax.invert_yaxis()  # y축 위로
 
         cmap = colors.ListedColormap(['black', 'white'])
         ax.imshow(array, cmap=cmap, extent=[max_row_hint, total_width, max_col_hint, total_height])
 
-
+        # 5. 힌트 텍스트 표시
         for i, hint in enumerate(row_hints):
             for j, num in enumerate(reversed(hint)):
                 ax.text(j + max_row_hint - len(hint), i + max_col_hint + 0.5, str(num), ha='center', va='center', fontsize=6)
--- nonogram/nonogram/settings.py
+++ nonogram/nonogram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z^xdhi5n#!(i!8p(kiuo*6dr$*bku&^hk!zp!r&sn4nm^4^_c9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
