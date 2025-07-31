2a5991692dfcf0fa3ca103b6c401391b0fb86d0e
nahyunho99 <nahyunho1999@gmail.com>
2025-08-01 00:00:08 +0900

    Delete cache

--- nonogram/nonogram/settings.py
+++ nonogram/nonogram/settings.py
@@ -28,7 +28,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'nonogram', 'media')
 SECRET_KEY = 'django-insecure-z^xdhi5n#!(i!8p(kiuo*6dr$*bku&^hk!zp!r&sn4nm^4^_c9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "140.245.71.233", "nonogram.duckdns.org"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')  # 추가
