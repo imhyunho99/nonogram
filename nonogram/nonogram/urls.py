61d84d4ae0e71f0c1f2526a6fda83cc69cebe88f
nahyunho99 <nahyunho1999@gmail.com>
2025-08-02 00:37:32 +0900

    Fix: setting.py, Title

--- nonogram/nonogram/urls.py
+++ nonogram/nonogram/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -25,6 +26,7 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('image/', include('image.urls')),
     path('suggestion/', include('suggestion.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 
 if settings.DEBUG:
