d25b5218584f16fd10a5c2fa65dfb0ddeadd3455
nahyunho99 <nahyunho1999@gmail.com>
2025-08-11 23:34:18 +0900

    Fix: Login 405, WIP: Seperate frontend, django be

--- nonogram/nonogram/urls.py
+++ nonogram/nonogram/urls.py
@@ -1,32 +1,19 @@
-"""
-URL configuration for nonogram project.
+# nonogram/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
+api_patterns = [
     path('user/', include('user.urls')),
     path('image/', include('image.urls')),
     path('suggestion/', include('suggestion.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(api_patterns)),
 ]
 
 if settings.DEBUG:
