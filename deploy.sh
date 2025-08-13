26f934ed18abb569d018d997f3cae7295d7b2ab3
nahyunho99 <nahyunho1999@gmail.com>
2025-08-13 17:14:03 +0900

    Auto Deploy .yml, .sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+APP_DIR="/home/ubuntu/nonogram/nonogram"
+VENV="/home/ubuntu/nonogram"
+
+cd $APP_DIR
+
+echo "📥 Git pull"
+git pull origin main
+
+echo "📦 가상환경 활성화"
+source $VENV/bin/activate
+
+echo "📂 Migration & Static"
+python manage.py migrate
+
+echo "♻️ 무중단 리로드"
+touch  /home/ubuntu/nonogram/reload.txt
+
+echo "🔄 uWSGI 재시작 (systemd)"
+sudo systemctl restart uwsgi
+
+echo "✅ 배포 완료"
+
