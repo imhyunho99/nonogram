6da3701f49bec42977a23da5f1325c4f167d18f8
nahyunho99 <nahyunho1999@gmail.com>
2025-08-24 17:43:25 +0900

    Test: Login time(Password hashers)

--- nonogram/nonogram/settings.py
+++ nonogram/nonogram/settings.py
@@ -113,6 +113,19 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher_SHA256',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
+PASSWORD_HASHING_ALGORITHM = {
+    'PBKDF2_ITERATIONS': 200000,
+}
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
