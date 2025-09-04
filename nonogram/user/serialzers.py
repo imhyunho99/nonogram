59f1f7a2c3a1b8a291f43308f0f9a41d4f4b07ca
nahyunho99 <nahyunho1999@gmail.com>
2025-09-04 16:30:45 +0900

    Fix: update user password

--- nonogram/user/serialzers.py
+++ nonogram/user/serialzers.py
@@ -31,7 +31,27 @@ class LoginSerializer(serializers.Serializer):
         raise serializers.ValidationError("Validation ERR")
 
 
+class UpdateUserView(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("기존 비밀번호가 일치하지 않습니다.")
+        return value
+
+    def validate_new_password(self, value):
+        password_validation.validate_password(value, self.context['request'].user)
+        return value
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['new_password'])
+        instance.save()
+        return instance
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ("id", "email", "username")
+
--- nonogram/user/urls.py
+++ nonogram/user/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-
-from .views import DeleteUserView, LoginView, LogoutView, RegisterView, UserDetailView
+from .views import DeleteUserView, LoginView, LogoutView, RegisterView, UserDetailView, UpdateUserView
 
 urlpatterns = [
 path("register/", RegisterView.as_view(), name="register"),
@@ -8,4 +7,5 @@ path("login/", LoginView.as_view(), name="login"),
 path("logout/", LogoutView.as_view(), name="logout"),
 path("me/", UserDetailView.as_view(), name="user-detail"),
 path("delete/", DeleteUserView.as_view(), name="user-delete"),
+path("update/", UpdateUserView.as_view(), name="user-update"),
     ]
